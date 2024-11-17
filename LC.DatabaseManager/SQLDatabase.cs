514bb55fc81ba5ae6965c1ba83082edf6b9b2320
Eraxle <eraxle.org@gmail.com>
2024-11-17 18:38:16 +0400

    native projects added

---
+++ LC.DatabaseManager/SQLDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LC.DatabaseManager
+{
+    public class SQLDatabase<T>
+    {
+
+    }
+}
---
+++ LC.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LC.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
